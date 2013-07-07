ec825c366c25331d3b3df80f075b599cd75475ce
Ubuntu <ubuntu@ip-172-31-34-64.us-west-2.compute.internal>
2013-07-07 16:05:34 +0000

    Added link to index.html

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSync(index.html))
+  var buf = fs.readFileSync('index.html'))
   console.log(buf);
   response.send(buf.toString());
 });
