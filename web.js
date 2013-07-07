1e0f16a0ff128fee73d9224128424ff9cb56d69d
Ubuntu <ubuntu@ip-172-31-34-64.us-west-2.compute.internal>
2013-07-07 17:13:11 +0000

    small change

--- web.js
+++ web.js
@@ -7,11 +7,12 @@ app.get('/', function(request, response) {
   var fs = require('fs');
   var content = fs.readFile('./index.html','utf8', function (err,data) {
     if (err) {
-      return console.log(err);
+     // return console.log(err);
+     response.send("Problem");
+    } else {
+     response.send("Booyah");
     }
-    console.log(data);
   });
-  response.send("Booyah");
 });
 
 var port = process.env.PORT || 5000;
